6c2655f8b4badcfb4eed7411197a3f3598bb795d
Tonni Ramdani <tonniramdani@gmail.com>
2024-10-29 17:08:20 +0700

    edit laporan_pdf

--- application/views/laporan_pdf.php
+++ application/views/laporan_pdf.php
@@ -1,39 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Print Mahasiswa</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Print Mahasiswa</title>
 </head>
 <body>
-    <table>
-        <tr>
-            <th>NO</th>
-            <th>NAMA MAHASISWA</th>
-            <th>NIM</th>
-            <th>TANGGAL LAHIR</th>
-            <th>JURUSAN</th>
-            <th>ALAMAT</th>
-            <th>EMAIL</th>
-            <th>NO TELEPON</th>
-        </tr>
-        <?php
-            $no = 1;
-            foreach ($mahasiswa as $mhs) : 
-        ?>
-            <tr>
-                <td><?php echo $no++; ?></td>
-                <td><?php echo $mhs->nama; ?></td>
-                <td><?php echo $mhs->nim; ?></td>
-                <td><?php echo $mhs->tgl_lahir; ?></td>
-                <td><?php echo $mhs->jurusan; ?></td>
-                <td><?php echo $mhs->alamat; ?></td>
-                <td><?php echo $mhs->email; ?></td>
-                <td><?php echo $mhs->no_telp; ?></td>
-            </tr>
-        <?php 
-            endforeach;
-        ?>
-    </table>   
+<table>
+<tr>
+<th>NO</th>
+<th>NAMA MAHASISWA</th>
+<th>NIM</th>
+<th>TANGGAL LAHIR</th>
+<th>JURUSAN</th>
+<th>ALAMAT</th>
+<th>EMAIL</th>
+<th>NO TELEPON</th>
+</tr>
+<?php
+$no = 1;
+foreach ($mahasiswa as $mhs) : 
+?>
+<tr>
+<td><?php echo $no++; ?></td>
+<td><?php echo $mhs->nama; ?></td>
+<td><?php echo $mhs->nim; ?></td>
+<td><?php echo $mhs->tgl_lahir; ?></td>
+<td><?php echo $mhs->jurusan; ?></td>
+<td><?php echo $mhs->alamat; ?></td>
+<td><?php echo $mhs->email; ?></td>
+<td><?php echo $mhs->no_telp; ?></td>
+</tr>
+<?php 
+endforeach;
+?>
+</table>   
 </body>
 </html>
